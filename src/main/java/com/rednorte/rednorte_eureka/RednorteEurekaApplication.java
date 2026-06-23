b0e98868c92ae2c4d70747cffae8941fcc010f32
Marcelo San Martín <ma.sanmartinu@duocuc.cl>
2026-06-22 23:50:09 -0400

    Eureka Server - Service Discovery para RedNorte microservicios

---
+++ src/main/java/com/rednorte/rednorte_eureka/RednorteEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.rednorte.rednorte_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednorteEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RednorteEurekaApplication.class, args);
+	}
+
+}
