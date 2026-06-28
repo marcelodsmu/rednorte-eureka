b286887e1dfd9e6fc520fec0928acc9ccd4df7bf
Marcelo San Martín <ma.sanmartinu@duocuc.cl>
2026-06-28 09:28:30 -0400

    feat: actualizar configuracion y arranque de eureka

--- src/main/java/com/rednorte/rednorte_eureka/RednorteEurekaApplication.java
+++ src/main/java/com/rednorte/rednorte_eureka/RednorteEurekaApplication.java
@@ -1,13 +1,13 @@
-package com.rednorte.rednorte_eureka;
+package com.rednorte.rednorte_eureka; // Tu package puede variar un poco, deja el que viene por defecto.
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RednorteEurekaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RednorteEurekaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RednorteEurekaApplication.class, args);
+    }
 }
